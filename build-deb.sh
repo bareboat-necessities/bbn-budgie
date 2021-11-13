ca2c582e418732a017b85a2f249cdd28d80fa233
mgrouch <mgrushinskiy@gmail.com>
2021-11-12 23:26:06 -0500

    initial

--- build-deb.sh
+++ build-deb.sh
@@ -10,7 +10,8 @@ apt-get -y install devscripts build-essential lintian \
    gir1.2-gee-0.8 gnome-common gobject-introspection gtk-doc-tools libgee-0.8-dev libgnome-bluetooth-dev \
    libgnome-desktop-3-dev libgnome-menu-3-dev libgtk-3-dev libmutter-3-dev  libpeas-dev libpolkit-agent-1-dev \
    libpolkit-gobject-1-dev libupower-glib-dev libwnck-3-dev libgdk-pixbuf2.0-dev libaccountsservice-dev valac \
-   gnome-settings-daemon-dev sassc libnotify-dev
+   gnome-settings-daemon-dev sassc libnotify-dev \
+   libpulse-dev libpulse-mainloop-glib0 libibus-1.0-dev meson libasound2-dev
 
 wget http://deb.debian.org/debian/pool/main/b/budgie-desktop/budgie-desktop_10.5.orig.tar.xz
 wget http://deb.debian.org/debian/pool/main/b/budgie-desktop/budgie-desktop_10.5-1.debian.tar.xz
