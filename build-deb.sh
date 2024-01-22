daf643e1f4b21c37ac297df0f117a1f3b357b4de
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2024-01-22 07:54:45 -0500

    Update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -8,7 +8,7 @@ apt-get upgrade -y -q
 
 apt-get -y install devscripts debhelper-compat build-essential lintian \
    gir1.2-gee-0.8 gnome-common gobject-introspection gtk-doc-tools libgee-0.8-dev libgnome-bluetooth-dev \
-   libgnome-desktop-3-dev libgnome-menu-3-dev libgtk-3-dev libmutter-7-dev  libpeas-dev libpolkit-agent-1-dev \
+   libgnome-desktop-3-dev libgnome-menu-3-dev libgtk-3-dev libmutter-11-dev  libpeas-dev libpolkit-agent-1-dev \
    libpolkit-gobject-1-dev libupower-glib-dev libwnck-3-dev libgdk-pixbuf2.0-dev libaccountsservice-dev valac \
    gnome-settings-daemon-dev sassc libnotify-dev gnome-screensaver libgstreamer1.0-dev uuid-dev \
    libpulse-dev libpulse-mainloop-glib0 libglib2.0-dev libibus-1.0-dev meson libasound2-dev
@@ -20,12 +20,12 @@ apt-get -y install devscripts debhelper-compat build-essential lintian \
 #cd budgie-desktop-10.5.2/
 #xzcat ../budgie-desktop_10.5.2-4.debian.tar.xz | tar xvf -
 
-wget https://launchpad.net/debian/+archive/primary/+sourcefiles/budgie-desktop/10.5.3-5/budgie-desktop_10.5.3-5.debian.tar.xz
-wget https://launchpad.net/debian/+archive/primary/+sourcefiles/budgie-desktop/10.5.3-5/budgie-desktop_10.5.3.orig.tar.xz
+wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/budgie-desktop/10.8.2-2ubuntu1/budgie-desktop_10.8.2-2ubuntu1.debian.tar.xz
+wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/budgie-desktop/10.8.2-2ubuntu1/budgie-desktop_10.8.2.orig.tar.xz
 
-xzcat budgie-desktop_10.5.3.orig.tar.xz | tar xvf -
-cd budgie-desktop-10.5.3/
-xzcat ../budgie-desktop_10.5.3-5.debian.tar.xz | tar xvf -
+xzcat budgie-desktop_10.8.2.orig.tar.xz | tar xvf -
+cd budgie-desktop-10.8.2/
+xzcat ../budgie-desktop_10.8.2-2ubuntu1.tar.xz | tar xvf -
 
 #cp ../debian/control debian/control
 
